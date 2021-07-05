2178b52de2c9fe02c818861807a2d9b7b55d5e6c
ramizqurbanli <79998135+ramizqurbanli@users.noreply.github.com>
2021-07-05 16:49:30 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ document.querySelector(".code").style.height=screen.height*0.3+"px";
 document.querySelector(".code").style.width= screen.width*0.85+"px";
 function compile() {
 	document.querySelector(".comp").srcdoc=document.querySelector(".code").value+"<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-}
+} 
 
